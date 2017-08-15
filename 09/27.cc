052485207facc8b2e8e6f2ba293cf12691f387b3
Xiaomu0 <w1724323603@163.com>
2017-08-15 18:28:22 +0800

    Added files: 09/27.cc 09/28.cc

---
+++ 09/27.cc
@@ -0,0 +1,25 @@
+#include <forward_list>
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::forward_list<int> ilst;
+  for (int i; std::cin >> i; ) {
+    ilst.push_front(i);  // reverse insertion
+  }
+  for (auto prev = ilst.before_begin(), curr = ilst.begin();
+       curr != ilst.end(); ) {
+    if (*curr % 2 == 1) {
+      curr = ilst.erase_after(prev);
+    } else {
+      ++prev;
+      ++curr;
+    }
+  }
+  // output testing
+  for (const auto &i : ilst) {
+    std::cout << i << ' ';
+  }
+  std::cout << std::endl;
+  return 0;
+}
---
+++ 09/28.cc
@@ -0,0 +1,44 @@
+#include <forward_list>
+#include <iostream>
+#include <string>
+
+using StringList = std::forward_list<std::string>;
+
+StringList::iterator ListInsertAfterString(
+    StringList &list, const std::string &to_find, const std::string &str) {
+  StringList::iterator prev = list.before_begin(), curr = list.begin();
+  while (curr != list.end()) {
+    if (*curr != to_find) {
+      ++prev;
+      ++curr;
+    } else {
+      ++prev;
+      ++curr;
+      break;
+    }
+  }
+  return curr = list.insert_after(prev, str);
+}
+
+int main() {
+  StringList slst;
+  std::string str;
+  auto prev = slst.before_begin(), curr = slst.begin();
+  std::cout << "Input some strings below, end up with EOF:" << std::endl;
+  while (std::cin >> str) {
+    prev = curr = slst.insert_after(prev, str);
+    ++curr;
+  }
+  std::cin.clear();
+  std::string to_find;
+  std::cout << "Input the string to insert after:" << std::endl;
+  std::cin >> to_find;
+  std::cout << "Input the string to insert:" << std::endl;
+  std::cin >> str;
+  ListInsertAfterString(slst, to_find, str);
+  for (const auto &s : slst) {
+    std::cout << s << ' ';
+  }
+  std::cout << std::endl;
+  return 0;
+}
