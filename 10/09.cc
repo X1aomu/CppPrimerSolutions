a64923cbcff72ec0fd987a82b51e937dcf4b0557
X1aomu <w1724323603@163.com>
2018-07-16 00:13:01 +0800

    Rewrite GNU Makefiles

--- 10/09.cc
+++ 10/09.cc
@@ -1,11 +1,32 @@
 #include <algorithm>
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+void print(const vector<string> &words) {
+  for (auto word : words) {
+    cout << word << " ";
+  }
+  cout << endl;
+}
+
 void EliminateDups(vector<string> &words) {
+  print(words);
   sort(words.begin(), words.end());
   auto end_unique = unique(words.begin(), words.end());
+  print(words);
   words.erase(end_unique, words.end());
+  print(words);
+}
+
+int main() {
+  vector<string> svec;
+  string word;
+  while (cin >> word) {
+    svec.push_back(word);
+  }
+  EliminateDups(svec);
+  return 0;
 }
